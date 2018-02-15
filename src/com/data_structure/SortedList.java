4c66522ddfa39cc8b0661952423be9ff37b700a5
Shivani Jain <shivani1723@gmail.com>
2018-02-15 23:46:15 +0530

    addition of node in sorted list

---
+++ src/com/data_structure/SortedList.java
@@ -0,0 +1,35 @@
+package com.data_structure;
+
+/**
+ * Created by Shivani on 15-02-2018.
+ */
+public class SortedList extends CustomiseList {
+
+    void insertSorted( int data ){
+        Node current = this.head;
+        int pos = 1;
+        while( current != null ) {
+          //  System.out.println( Integer.parseInt( current.data ) + "");
+            if( data > Integer.parseInt( current.data ) && data < Integer.parseInt( current.next.data ) ) {
+                pos++;
+                break;
+            }
+            current = current.next;
+            pos++;
+        }
+        System.out.println( "Inserted Position" + pos );
+        this.head = insertNode( this.head, String.valueOf(data), pos);
+    }
+
+    public static void main(String[] args) {
+        SortedList ls = new SortedList();
+        ls.head = ls.insertNode( ls.head, "1",1);
+        ls.head = ls.insertNode( ls.head, "2",2 );
+        ls.head = ls.insertNode( ls.head, "3",3 );
+        ls.head = ls.insertNode( ls.head, "7",4 );
+        ls.head = ls.insertNode( ls.head, "10",5 );
+        ls.traverseList(ls.head);
+        ls.insertSorted(5);
+        ls.traverseList(ls.head);
+    }
+}
