c2c53ca2601f44e16fc54ce86b8ee8b593c694d2
Shivani Jain <shivani1723@gmail.com>
2018-02-16 00:01:47 +0530

    addition of node in sorted list bug fixes

--- src/com/data_structure/SortedList.java
+++ src/com/data_structure/SortedList.java
@@ -8,16 +8,15 @@ public class SortedList extends CustomiseList {
     void insertSorted( int data ){
         Node current = this.head;
         int pos = 1;
-        while( current != null ) {
+        while( current.next != null ) {
           //  System.out.println( Integer.parseInt( current.data ) + "");
             if( data > Integer.parseInt( current.data ) && data < Integer.parseInt( current.next.data ) ) {
-                pos++;
                 break;
             }
             current = current.next;
             pos++;
         }
-        System.out.println( "Inserted Position" + pos );
+        System.out.println( "Inserted Position" + pos++ );
         this.head = insertNode( this.head, String.valueOf(data), pos);
     }
 
@@ -26,10 +25,11 @@ public class SortedList extends CustomiseList {
         ls.head = ls.insertNode( ls.head, "1",1);
         ls.head = ls.insertNode( ls.head, "2",2 );
         ls.head = ls.insertNode( ls.head, "3",3 );
-        ls.head = ls.insertNode( ls.head, "7",4 );
+        ls.head = ls.insertNode( ls.head, "5",4 );
         ls.head = ls.insertNode( ls.head, "10",5 );
         ls.traverseList(ls.head);
-        ls.insertSorted(5);
+        ls.insertSorted(4);
+        ls.insertSorted( 11 );
         ls.traverseList(ls.head);
     }
 }
